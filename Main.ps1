019103614732c906d46f5cf3ae1b5a57bb383f1a
Gabi <182965942+GabiNun@users.noreply.github.com>
2025-08-05 20:54:03 -0700

    Update Main.ps1

--- Main.ps1
+++ Main.ps1
@@ -1,5 +1,5 @@
 irm raw.githubusercontent.com/GabiNun/Windows-Defender-Remover/main/Defender.reg -OutFile $Env:Temp\Defender.reg
-$a = New-ScheduledTaskAction -Execute 'reg.exe' -Argument 'import $env:TEMP\Defender.reg'
+$a = New-ScheduledTaskAction -Execute 'reg.exe' -Argument "import `"$env:TEMP\Defender.reg`""
 Register-ScheduledTask -TaskName 'ImportDefenderReg' -Action $a | Out-Null
 $svc = New-Object -ComObject 'Schedule.Service'
 $svc.Connect()
