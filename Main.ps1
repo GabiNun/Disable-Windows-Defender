76f940d3d6dceb4c1ff6e8171c079cc62683fcb4
Gabi <182965942+GabiNun@users.noreply.github.com>
2025-08-05 20:29:26 -0700

    Update Main.ps1

--- Main.ps1
+++ Main.ps1
@@ -1,1 +1,1 @@
-irm github.com/mbcdev/RunAsTrustedInstaller/releases/download/release-0.1/RunAsTI.exe -OutFile $Env:Temp\RunAsTi.exe;& $Env:Temp\RunAsTi.exe "C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe" -c "irm raw.githubusercontent.com/GabiNun/Windows-Defender-Remover/main/Defender.ps1 | iex"
+irm github.com/GabiNun/Windows-Defender-Remover/raw/main/PowerRun.exe -OutFile $Env:Temp\PowerRun.exe;& "$Env:Temp\PowerRun.exe" powershell.exe -Command "irm raw.githubusercontent.com/GabiNun/Windows-Defender-Remover/main/Defender.ps1 | iex"
