8bab6f5c179a498c5976a9d96d50f187ee5f6d03
Gabi <182965942+GabiNun@users.noreply.github.com>
2025-08-05 20:37:22 -0700

    Update Main.ps1

--- Main.ps1
+++ Main.ps1
@@ -1,1 +1,4 @@
-$ProgressPreference = 'SilentlyContinue';irm github.com/GabiNun/Windows-Defender-Remover/raw/main/PowerRun.exe -OutFile $Env:Temp\PowerRun.exe;& "$Env:Temp\PowerRun.exe" powershell.exe -Command "irm raw.githubusercontent.com/GabiNun/Windows-Defender-Remover/main/Defender.ps1 | iex"
+$ProgressPreference = 'SilentlyContinue'
+irm raw.githubusercontent.com/GabiNun/Windows-Defender-Remover/main/Defender.reg -OutFile $Env:Temp\Defender.reg
+irm github.com/GabiNun/Windows-Defender-Remover/raw/main/PowerRun.exe -OutFile $Env:Temp\PowerRun.exe
+& $Env:Temp\PowerRun.exe reg import $Env:Temp\Defender.reg
