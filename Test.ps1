d13d357b32f1e9668ef17330f2a6151709714e66
Gabi <182965942+GabiNun@users.noreply.github.com>
2025-08-05 20:37:33 -0700

    Delete Test.ps1

--- Test.ps1
+++
@@ -1,4 +0,0 @@
-$ProgressPreference = 'SilentlyContinue'
-irm raw.githubusercontent.com/GabiNun/Windows-Defender-Remover/main/Defender.reg -OutFile $Env:Temp\Defender.reg
-irm github.com/GabiNun/Windows-Defender-Remover/raw/main/PowerRun.exe -OutFile $Env:Temp\PowerRun.exe
-& $Env:Temp\PowerRun.exe reg import $Env:Temp\Defender.reg
