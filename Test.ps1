9724829ab1a3ffbb4e7f56f979e95e0b9d31d26b
Gabi <182965942+GabiNun@users.noreply.github.com>
2025-08-05 20:22:16 -0700

    Update Test.ps1

--- Test.ps1
+++ Test.ps1
@@ -1,1 +1,1 @@
-& "$Env:Temp\PsExec.exe" -s powershell.exe -Command "irm raw.githubusercontent.com/GabiNun/Windows-Defender-Remover/main/Defender.ps1 | iex"
+irm github.com/GabiNun/Windows-Defender-Remover/raw/main/PsExec.exe -OutFile $Env:Temp\PsExec.exe;& "$Env:Temp\PsExec.exe" -s powershell.exe -Command "irm raw.githubusercontent.com/GabiNun/Windows-Defender-Remover/main/Defender.ps1 | iex"
