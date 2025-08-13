db7f67bbf3bcdc4b4293c6bd819f1e42fbb4aca4
Gabi <182965942+GabiNun@users.noreply.github.com>
2025-08-12 20:43:57 -0700

    Update Main.ps1

--- Main.ps1
+++ Main.ps1
@@ -2,4 +2,11 @@ irm raw.githubusercontent.com/ionuttbara/windows-defender-remover/main/Remove_De
 Register-ScheduledTask -TaskName 'ImportDefenderReg' -Action (New-ScheduledTaskAction -Execute 'reg.exe' -Argument "import `"$env:TEMP\Defender.reg`"") | Out-Null
 ($s = New-Object -ComObject 'Schedule.Service').Connect(); $s.GetFolder('\').GetTask('ImportDefenderReg').RunEx($null, 0, 0, 'NT SERVICE\TrustedInstaller') | Out-Null
 
-$store = 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Appx\AppxAllUserStore'; $appx = Get-AppxPackage -Name Microsoft.SecHealthUI; $sids = @('S-1-5-18'); $sids += Get-ChildItem $store -ea 0 | % { $_.PSChildName } | ? { $_.StartsWith('S-1-5-21') }; New-Item -Path "$store\Deprovisioned\$($appx.PackageFamilyName)" -ItemType RegistryKey -Force | Out-Null; foreach ($sid in $sids) { New-Item -Path "$store\EndOfLife\$sid\$($appx.PackageFullName)" -ItemType RegistryKey -Force | Out-Null }; $appx | Remove-AppxPackage
+$store = 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Appx\AppxAllUserStore'
+$appx = Get-AppxPackage -AllUsers -Name "Microsoft.SecHealthUI" 
+$sids = @('S-1-5-18')
+
+if (Test-Path $store) { $sids += Get-ChildItem $store -ea 0 | %{ $_.PSChildName } | ?{ $_.StartsWith('S-1-5-21') } }
+New-Item "$store\Deprovisioned\$($appx.PackageFamilyName)" -Force | Out-Null
+foreach ($sid in $sids) { New-Item "$store\EndOfLife\$sid\$($appx.PackageFullName)" -Force | Out-Null }
+$appx | Remove-AppxPackage
