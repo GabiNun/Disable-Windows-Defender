c6d65ab8caacd9fe43426149187a3412a4c003aa
Gabi <182965942+GabiNun@users.noreply.github.com>
2025-08-13 23:15:12 -0700

    Update Main.ps1

--- Main.ps1
+++ Main.ps1
@@ -2,7 +2,7 @@ irm raw.githubusercontent.com/ionuttbara/windows-defender-remover/main/Remove_De
 Register-ScheduledTask -TaskName 'ImportDefenderReg' -Action (New-ScheduledTaskAction -Execute 'reg.exe' -Argument "import `"$env:TEMP\Defender.reg`"") | Out-Null
 ($s = New-Object -ComObject 'Schedule.Service').Connect(); $s.GetFolder('\').GetTask('ImportDefenderReg').RunEx($null, 0, 0, 'NT SERVICE\TrustedInstaller') | Out-Null
 
-$store = 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Appx\AppxAllUserStore';$appx = Get-AppxPackage -AllUsers -Name "Microsoft.SecHealthUI" ;$sids = @('S-1-5-18')
+$store = 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Appx\AppxAllUserStore';$appx = Get-AppxPackage -AllUsers -Name "Microsoft.SecHealthUI";$sids = @('S-1-5-18')
 
 if (Test-Path $store) { $sids += Get-ChildItem $store -ea 0 | %{ $_.PSChildName } | ?{ $_.StartsWith('S-1-5-21') } }
 New-Item "$store\Deprovisioned\$($appx.PackageFamilyName)" -Force | Out-Null
