86a8711f629082ad430a030342d3de1618600c51
Gabi <182965942+GabiNun@users.noreply.github.com>
2025-08-14 00:12:52 -0700

    Update Main.ps1

--- Main.ps1
+++ Main.ps1
@@ -1,5 +1,5 @@
 irm raw.githubusercontent.com/ionuttbara/windows-defender-remover/main/Remove_Defender/RemoveDefender.reg -o $Env:Temp\Defender.reg
-Register-ScheduledTask 'ImportDefenderReg' (New-ScheduledTaskAction reg.exe "import `"$env:TEMP\Defender.reg`"") | Out-Null
+Register-ScheduledTask -TaskName 'ImportDefenderReg' -Action (New-ScheduledTaskAction -Execute 'reg.exe' -Argument "import "$env:TEMP\Defender.reg"") | Out-Null
 ($s = New-Object -ComObject 'Schedule.Service').Connect(); $s.GetFolder('\').GetTask('ImportDefenderReg').RunEx($null, 0, 0, 'NT SERVICE\TrustedInstaller') | Out-Null
 
 $store = 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Appx\AppxAllUserStore';$appx = Get-AppxPackage Microsoft.SecHealthUI;$sids = @('S-1-5-18')
