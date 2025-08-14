d5028d3dc65d5a496a8fedb11571cbd9c0c8c4bd
Gabi <182965942+GabiNun@users.noreply.github.com>
2025-08-13 23:30:10 -0700

    Update Main.ps1

--- Main.ps1
+++ Main.ps1
@@ -4,6 +4,6 @@ Register-ScheduledTask -TaskName 'ImportDefenderReg' -Action (New-ScheduledTaskA
 
 $store = 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Appx\AppxAllUserStore';$appx = Get-AppxPackage -AllUsers -Name "Microsoft.SecHealthUI";$sids = @('S-1-5-18')
 
-if (Test-Path $store) { $sids += Get-ChildItem $store -ea 0 | %{ $_.PSChildName } | ?{ $_.StartsWith('S-1-5-21') } }
+$sids += Get-ChildItem $store -ea 0 | %{ $_.PSChildName } | ?{ $_.StartsWith('S-1-5-21') }
 foreach ($sid in $sids) { New-Item "$store\EndOfLife\$sid\$($appx.PackageFullName)" -Force | Out-Null }
 $appx | Remove-AppxPackage
