1b97ac0a341cfba8aa9bc4ef44ede5eb94e9c878
Gabi <182965942+GabiNun@users.noreply.github.com>
2025-08-14 00:01:36 -0700

    Update Test.ps1

--- Test.ps1
+++ Test.ps1
@@ -1,2 +1,6 @@
-$store = 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Appx\AppxAllUserStore';$appx = Get-AppxPackage Microsoft.SecHealthUI
-('S-1-5-18'+(Get-ChildItem $store -ea 0|% PSChildName|?{$_ -like 'S-1-5-21*'}))|%{New-Item "$store\EndOfLife\$_\$($appx.PackageFullName)" -Force|Out-Null};$appx | Remove-AppxPackage
+$store='HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Appx\AppxAllUserStore'
+$appx=Get-AppxPackage Microsoft.SecHealthUI
+
+('S-1-5-18'+(Get-ChildItem $store -ea 0|% PSChildName|?{$_ -like 'S-1-5-21*'}))|%{New-Item "$store\EndOfLife\$_\$($appx.PackageFullName)" -Force|Out-Null}
+
+$appx|Remove-AppxPackage
