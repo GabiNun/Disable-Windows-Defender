ffb9c263923bf51677df3d1266d46602e1fa31fa
Gabi <182965942+GabiNun@users.noreply.github.com>
2025-08-13 23:33:45 -0700

    Update Main.ps1

--- Main.ps1
+++ Main.ps1
@@ -2,7 +2,7 @@ irm raw.githubusercontent.com/ionuttbara/windows-defender-remover/main/Remove_De
 Register-ScheduledTask -TaskName 'ImportDefenderReg' -Action (New-ScheduledTaskAction -Execute 'reg.exe' -Argument "import `"$env:TEMP\Defender.reg`"") | Out-Null
 ($s = New-Object -ComObject 'Schedule.Service').Connect(); $s.GetFolder('\').GetTask('ImportDefenderReg').RunEx($null, 0, 0, 'NT SERVICE\TrustedInstaller') | Out-Null
 
-$store = 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Appx\AppxAllUserStore';$appx = Get-AppxPackage -AllUsers -Name "Microsoft.SecHealthUI";$sids = @('S-1-5-18')
+$store = 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Appx\AppxAllUserStore';$appx = Get-AppxPackage Microsoft.SecHealthUI;$sids = @('S-1-5-18')
 
 $sids += Get-ChildItem $store -ea 0 | % PSChildName | ?{ $_ -like 'S-1-5-21*' }
 $sids | % { New-Item "$store\EndOfLife\$_\$($appx.PackageFullName)" -Force | Out-Null }
