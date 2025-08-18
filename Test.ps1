bb58db5b8e431ffacd2eac10aaf248278ccd418b
Gabi <182965942+GabiNun@users.noreply.github.com>
2025-08-18 16:36:44 -0700

    Delete Test.ps1

--- Test.ps1
+++
@@ -1,4 +0,0 @@
-$store = 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Appx\AppxAllUserStore'
-$sids = @('S-1-5-18') + (gci $store -ea 0 | % PSChildName | ?{ $_ -like 'S-1-5-21*' })
-$sids | % { ni "$store\EndOfLife\$_\Microsoft.SecHealthUI_8wekyb3d8bbwe" -Fo | Out-Null }
-Get-AppxPackage Microsoft.SecHealthUI | Remove-AppxPackage
