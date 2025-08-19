65d02a9c0a7d21a581934831d39f601b94dfb5ca
Gabi <182965942+GabiNun@users.noreply.github.com>
2025-08-18 22:53:06 -0700

    Delete Test.ps1

--- Test.ps1
+++
@@ -1,3 +0,0 @@
-irm raw.githubusercontent.com/ionuttbara/windows-defender-remover/main/Remove_Defender/RemoveDefender.reg -o $Env:Temp\Defender.reg
-Register-ScheduledTask ImportDefenderReg -Action (New-ScheduledTaskAction reg.exe "import $env:TEMP\Defender.reg")|Out-Null
-$o=New-Object -Com 'Schedule.Service';$o.Connect();$o.GetFolder('\').GetTask('ImportDefenderReg').RunEx($null,0,0,'NT SERVICE\TrustedInstaller')|Out-Null
