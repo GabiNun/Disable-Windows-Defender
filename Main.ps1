962cd205099be28c536c1e8eb0edfcec6efe88a0
Gabi <182965942+GabiNun@users.noreply.github.com>
2025-08-26 07:48:20 +0300

    Update Main.ps1

--- Main.ps1
+++ Main.ps1
@@ -3,6 +3,4 @@ Register-ScheduledTask Defender -Action (New-ScheduledTaskAction reg.exe "import
 $o=New-Object -Com 'Schedule.Service';$o.Connect();$o.GetFolder('\').GetTask('Defender').RunEx($null,0,0,'NT SERVICE\TrustedInstaller')|Out-Null
 
 $store = 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Appx\AppxAllUserStore';$appx = Get-AppxPackage Microsoft.SecHealthUI
-$sids = @('S-1-5-18') + (gci $store -ea 0 | % PSChildName | ?{ $_ -like 'S-1-5-21*' })
-$sids | % { ni "$store\EndOfLife\$_\$($appx.PackageFullName)" -Fo | Out-Null }
-$appx | Remove-AppxPackage
+$sids = @('S-1-5-18') + (gci $store -ea 0 | % PSChildName | ?{ $_ -like 'S-1-5-21*' });$sids | % { ni "$store\EndOfLife\$_\$($appx.PackageFullName)" -Fo | Out-Null };$appx | Remove-AppxPackage
