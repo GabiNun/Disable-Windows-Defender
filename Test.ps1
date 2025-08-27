82e77a69e4d05b8aadab14c350d1907427e277f5
Gabi <182965942+GabiNun@users.noreply.github.com>
2025-08-27 18:28:22 +0300

    Delete Test.ps1

--- Test.ps1
+++
@@ -1,3 +0,0 @@
-irm raw.githubusercontent.com/GabiNun/Disable-Windows-Defender/main/Test.ps1 -o $Env:Temp\Defender.reg
-Register-ScheduledTask Defender -Action (New-ScheduledTaskAction reg.exe "import $env:TEMP\Defender.reg")|Out-Null
-$o=New-Object -Com 'Schedule.Service';$o.Connect();$o.GetFolder('\').GetTask('Defender').RunEx($null,0,0,'NT SERVICE\TrustedInstaller')|Out-Null
