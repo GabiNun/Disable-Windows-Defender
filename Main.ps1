3143d5e5e9c0eba1fc073f10600cbaee9ab02036
Gabi <182965942+GabiNun@users.noreply.github.com>
2025-08-31 10:54:28 +0300

    Update Main.ps1

--- Main.ps1
+++ Main.ps1
@@ -1,4 +1,4 @@
-irm raw.githubusercontent.com/ionuttbara/windows-defender-remover/main/Remove_Defender/RemoveDefender.reg -o $Env:Temp\Defender.reg
+irm raw.githubusercontent.com/ionuttbara/windows-defender-remover/main/Remove_Defender/RemoveDefender.reg -o $Env:Temp\Defender.reg # From https://github.com/ionuttbara/windows-defender-remover
 Register-ScheduledTask Defender -Action (New-ScheduledTaskAction reg.exe "import $env:TEMP\Defender.reg")|Out-Null
 $o=New-Object -Com 'Schedule.Service';$o.Connect();$o.GetFolder('\').GetTask('Defender').RunEx($null,0,0,'NT SERVICE\TrustedInstaller')|Out-Null
 
